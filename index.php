edee679caf667097f1316c8add717b4112424d34
liebekinder <sebast_44@hotmail.fr>
2013-09-24 19:03:20 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Mon gestionnaire de fichier</title>
+	<meta name="description" content="Gestionnaire de fichier distant">
+	<meta name="author" content="liebekinder">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+	<table id="generaltable">
+		<tr id="header">
+			<td>
+			
+			</td>
+		</tr>
+		<tr id="content">
+			<td></td>
+		</tr>
+	</table>
+
+	<script src="js/scripts.js"></script>
+</body>
+</html>
